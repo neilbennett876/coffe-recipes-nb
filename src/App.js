f1f88a27121903dead705da6a389b9c80a7fac44
Neil Bennett <neil.bennett876@Neils-MacBook-Air.local>
2022-05-17 15:16:49 -0400

    added hot and cold buttons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import CCoffee from "./components/CCoffee";
+// import CCoffee from "./components/CCoffee";
 import FCoffee from "./components/FCoffee";
 
 function App() {
   return (
     <>
-    <CCoffee firstName ="Neil" />
+    {/* <CCoffee firstName ="Neil" /> */}
     <FCoffee firstName ="Neil" />
     </>
   );
--- src/components/FCoffee.js
+++ src/components/FCoffee.js
@@ -1,16 +1,32 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 
 function FCoffee ({firstName}) {
-    const [count, setCount] = useState(0)
-    const handleClick = () => {
-        setCount(count + 1)
-    }
+    const [coffeeList, setCoffeeList] = useState()
+    const [temperature, setTemperature] = useState('iced')
+    useEffect(() => {
+        fetch(`https://api.sampleapis.com/coffee/${temperature}`)
+            .then(response => response.json())
+            .then(data => setCoffeeList(data))
+            // .then(setCoffeeList) Same as line above
+            .catch(console.error)
+    }, [temperature])
     return (
         <>
             <h1>Coffee List (F)</h1>
             <p>Hello {firstName}</p>
-            <p>You clicked the button {count} times.</p>
-            <button onClick={handleClick}>Click me!!</button>
+            <button onClick={()=>setTemperature('hot')}>hot🔥</button>
+            <button onClick={()=>setTemperature('iced')}>iced❄️</button>
+            {!coffeeList
+                ? <h2>Loading🤔</h2>
+                :<>
+                <h2>Coffees ☕️</h2>
+                {coffeeList.map(coffee => {
+                    return <p key={coffee.id}>{coffee.title}</p>
+                }
+                )}
+                
+                </>
+            }
         </>
     )
 }
